cb9cef6e88fb6fe4ef3fd3e93f91a2b0528962fb
Benntobox <benjaminsboyle3@gmail.com>
2020-08-27 19:37:52 -0700

    Finished _.contains

--- src/underbar.js
+++ src/underbar.js
@@ -205,6 +205,7 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    
   };
 
   /*************************************** */
